e1ab5898a52956d9e6e396d9f9e71bb50c48ade3
Ashish Gupta <akvns152@gmail.com>
2018-02-26 23:21:23 +0530

    front end

--- pages/home.php
+++ pages/home.php
@@ -183,8 +183,7 @@
       <div class="menu">
         <a href="../index.php"><button><div class="btn-text">HOME</div></button></a><br><br><br>
            <a href="events.php"><button><div class="btn-text">EVENTS</div></button></a><br><br><br>
-           <a href="registrations.php"><button><div class="btn-text">REGISTRATION</div></button></a><br><br><br>
-           <a href="login.php"><button><div class="btn-text">LOGIN</div></button></a><br><br><br>
+           <a href="logout.php"><button><div class="btn-text">LOGOUT</div></button></a><br><br><br>
       </div>
     </div>
 </body>
